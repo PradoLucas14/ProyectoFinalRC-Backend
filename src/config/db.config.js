1bfc35d188cb32e8ce6f942315ed0f02c05e58da
Prado Lucas Nahuel <lukasnahuelprado32@gmail.com>
2024-06-25 21:57:06 -0300

    se conecto la base de datos hacien una modificacion a los archivos db.config.js y .env

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const connectDB = async () => {
+async function connectDB() {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          // Otras opciones según sea necesario
         });
-        console.log('Conectado a MongoDB');
-    } catch (error) {
-        console.error('Error de conexión a MongoDB:', error.message);
-        process.exit(1);  // Detener la aplicación en caso de error
+        console.log("Conexión a MongoDB establecida correctamente");
+      } catch (error) {
+        console.error("Error al conectar a MongoDB:", error);
+        process.exit(1); // Detiene el proceso de Node.js si hay un error de conexión
+      }
     }
-};
 
 module.exports = connectDB;
